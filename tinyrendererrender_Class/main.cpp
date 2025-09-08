37adff86f22adeeaf368a8a30ae4b45414f3438c
Yujunlin <1192056191@qq.com>
2025-09-08 23:41:07 +0800

    Init

---
+++ tinyrendererrender_Class/main.cpp
@@ -0,0 +1,24 @@
+#include "tgaimage.h"
+
+constexpr TGAColor white = { 255, 255, 255, 255 }; // attention, BGRA order
+constexpr TGAColor green = { 0, 255,   0, 255 };
+constexpr TGAColor red = { 0,   0, 255, 255 };
+constexpr TGAColor blue = { 255, 128,  64, 255 };
+constexpr TGAColor yellow = { 0, 200, 255, 255 };
+
+int main(int argc, char** argv) {
+    constexpr int width = 64;
+    constexpr int height = 64;
+    TGAImage framebuffer(width, height, TGAImage::RGB);
+
+    int ax = 7, ay = 3;
+    int bx = 12, by = 37;
+    int cx = 62, cy = 53;
+
+    framebuffer.set(ax, ay, red);
+    framebuffer.set(bx, by, white);
+    framebuffer.set(cx, cy, white);
+
+    framebuffer.write_tga_file("framebuffer.tga");
+    return 0;
+}
